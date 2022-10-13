e754894817cafd1069f52446e72a0f242ea9d021
HeindyLLaurore <95661415+HeindyLLaurore@users.noreply.github.com>
2022-10-12 21:11:24 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import email
-from fl requireask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 import csv
 
 app = Flask(__name__)
